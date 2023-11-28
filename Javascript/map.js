785079b83cc7b6139bcf263e6166d6fe6d30d7b7
Roy <f0980774144@gmail.com>
2023-11-29 00:26:01 +0800

    增加站點icon顯示

--- Javascript/map.js
+++ Javascript/map.js
@@ -24,6 +24,38 @@ let greenIcon = new L.Icon({
   shadowSize: [41, 41],
 });
 
+let blackIcon = new L.Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+let VioletIcon = new L.Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+let greyIcon = new L.Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-grey.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
 // 渲染站點到地圖
 function renderMap(data) {
   let map = L.map("map").setView(userPosition, 17);
@@ -32,15 +64,24 @@ function renderMap(data) {
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
 
-  L.marker(userPosition, { icon: greenIcon })
+  L.marker(userPosition, { icon: VioletIcon })
     .addTo(map)
     .bindPopup(`<p>您的位置</p>`)
     .openPopup();
 
   let markers = L.markerClusterGroup().addTo(map);
   data.forEach(function (item) {
+    let mask;
+    if (item.bemp == 0) {
+      mask = blackIcon;
+    } else if (item.sbi == 0) {
+      mask = greyIcon;
+    } else {
+      mask = greenIcon;
+    }
+
     markers.addLayer(
-      L.marker([item.lat, item.lng])
+      L.marker([item.lat, item.lng], { icon: mask })
         .bindPopup(
           `<p>站名：${item.sna}</p><p>地址：${item.ar}</p><p>可租借車輛：${item.sbi}</p><p>可歸還車位：${item.bemp}</p>`
         )
