fff38c7d9a2a698552ace35818a7c8df78237c97
Roy <f0980774144@gmail.com>
2023-11-28 17:37:15 +0800

    新增路名篩選及地圖搜尋

--- Javascript/all.js
+++ Javascript/all.js
@@ -10,6 +10,7 @@ axios
     filterData(originalData);
     keywordSearch(originalData);
     renderMrtList(originalData);
+    renderRoadSelect(originalData);
     renderList();
   });
 
@@ -22,29 +23,265 @@ function renderData(data) {
   data.forEach(function (item, index) {
     // console.log(item);
     let content = `<li class="fw-bold bg-dark text-white
-    border border-warning border-3 rounded-4 py-3 mb-3">站名：${item.sna}<p>地址：${item.ar}</p><br>站點目前可租借車輛<p class="fs-3 text-warning">${item.sbi}</p>可歸還車位<p class="fs-3 text-warning">${item.bemp}</p></li>`;
+    border border-warning border-3 rounded-4 py-3 mb-3">站名：${item.sna}<p>地址：${item.ar}</p><br>站點目前可租借車輛<p class="fs-3 text-warning">${item.sbi}</p>可歸還車位<p class="fs-3 text-warning mb-0">${item.bemp}</p></li>`;
     str += content;
   });
   list.innerHTML = str;
   countData.innerHTML = `－共 <span class="fw-bold">${data.length}</span> 處租借站－`;
 }
 
-//篩選邏輯
+//區域及道路篩選邏輯
+let taipei_roads = [
+  {
+    區域: "中正區",
+    road: [
+      { 道路名稱: "忠孝西路" },
+      { 道路名稱: "忠孝東路" },
+      { 道路名稱: "重慶南路" },
+      { 道路名稱: "重慶北路" },
+      { 道路名稱: "中正路" },
+      { 道路名稱: "杭州南路" },
+      { 道路名稱: "杭州北路" },
+      { 道路名稱: "南昌南路" },
+      { 道路名稱: "南昌北路" },
+      { 道路名稱: "延平南路" },
+      { 道路名稱: "市民大道" },
+      { 道路名稱: "南海路" },
+      { 道路名稱: "懷寧街" },
+      { 道路名稱: "仁愛路" },
+      { 道路名稱: "愛國西路" },
+      { 道路名稱: "和平西路" },
+      { 道路名稱: "南昌街" },
+      { 道路名稱: "中華路" },
+    ],
+  },
+  {
+    區域: "大同區",
+    road: [
+      { 道路名稱: "民生西路" },
+      { 道路名稱: "重慶北路" },
+      { 道路名稱: "承德路" },
+      { 道路名稱: "民權西路" },
+      { 道路名稱: "大龍街" },
+      { 道路名稱: "延平北路" },
+      { 道路名稱: "迪化街" },
+      { 道路名稱: "甘泉街" },
+      { 道路名稱: "赤峰街" },
+      { 道路名稱: "南京西路" },
+      { 道路名稱: "赤峰街" },
+    ],
+  },
+  {
+    區域: "中山區",
+    road: [
+      { 道路名稱: "中山北路" },
+      { 道路名稱: "林森北路" },
+      { 道路名稱: "林森南路" },
+      { 道路名稱: "民權東路" },
+      { 道路名稱: "民權西路" },
+      { 道路名稱: "建國北路" },
+      { 道路名稱: "建國南路" },
+      { 道路名稱: "中山東路" },
+      { 道路名稱: "中山西路" },
+      { 道路名稱: "中山北路二段" },
+      { 道路名稱: "中山南路" },
+      { 道路名稱: "長春路" },
+      { 道路名稱: "南京西路" },
+      { 道路名稱: "南京東路" },
+      { 道路名稱: "松江路" },
+      { 道路名稱: "南京東路一段" },
+    ],
+  },
+  {
+    區域: "松山區",
+    road: [
+      { 道路名稱: "敦化北路" },
+      { 道路名稱: "敦化南路" },
+      { 道路名稱: "八德路" },
+      { 道路名稱: "南京西路" },
+      { 道路名稱: "南京東路" },
+      { 道路名稱: "民生東路" },
+      { 道路名稱: "民生西路" },
+      { 道路名稱: "市民大道" },
+      { 道路名稱: "東興路" },
+      { 道路名稱: "松隆路" },
+      { 道路名稱: "光復北路" },
+      { 道路名稱: "三民路" },
+      { 道路名稱: "建國北路一段" },
+      { 道路名稱: "松仁路" },
+    ],
+  },
+  {
+    區域: "大安區",
+    road: [
+      { 道路名稱: "忠孝西路" },
+      { 道路名稱: "忠孝東路" },
+      { 道路名稱: "仁愛路" },
+      { 道路名稱: "信義路" },
+      { 道路名稱: "羅斯福路" },
+      { 道路名稱: "復興南路" },
+      { 道路名稱: "復興北路" },
+      { 道路名稱: "安和路" },
+      { 道路名稱: "辛亥路" },
+      { 道路名稱: "和平東路" },
+      { 道路名稱: "建國南路" },
+      { 道路名稱: "新生南路" },
+      { 道路名稱: "新生北路" },
+      { 道路名稱: "金華街" },
+      { 道路名稱: "臺大公館校區" },
+    ],
+  },
+  {
+    區域: "萬華區",
+    road: [
+      { 道路名稱: "西寧南路" },
+      { 道路名稱: "西寧北路" },
+      { 道路名稱: "漢中街" },
+      { 道路名稱: "青年路" },
+      { 道路名稱: "西園路" },
+      { 道路名稱: "昆明街" },
+      { 道路名稱: "和平西路" },
+      { 道路名稱: "康定路" },
+      { 道路名稱: "萬大路" },
+    ],
+  },
+  {
+    區域: "信義區",
+    road: [
+      { 道路名稱: "信義路" },
+      { 道路名稱: "松高路" },
+      { 道路名稱: "基隆路" },
+      { 道路名稱: "敦化南路" },
+      { 道路名稱: "敦化北路" },
+      { 道路名稱: "光復南路" },
+      { 道路名稱: "光復北路" },
+      { 道路名稱: "和平東路" },
+      { 道路名稱: "松仁路" },
+      { 道路名稱: "市民大道" },
+      { 道路名稱: "忠孝東路四段" },
+    ],
+  },
+  {
+    區域: "士林區",
+    road: [
+      { 道路名稱: "中山北路" },
+      { 道路名稱: "承德路" },
+      { 道路名稱: "文昌路" },
+      { 道路名稱: "天母南路" },
+      { 道路名稱: "天母北路" },
+      { 道路名稱: "忠誠路" },
+      { 道路名稱: "永平街" },
+      { 道路名稱: "福港街" },
+      { 道路名稱: "德行東路" },
+    ],
+  },
+  {
+    區域: "北投區",
+    road: [
+      { 道路名稱: "石牌路" },
+      { 道路名稱: "中央北路" },
+      { 道路名稱: "東華街" },
+      { 道路名稱: "中央南路" },
+      { 道路名稱: "立德街" },
+      { 道路名稱: "裕民街" },
+      { 道路名稱: "立功街" },
+      { 道路名稱: "中央東路" },
+    ],
+  },
+  {
+    區域: "內湖區",
+    road: [
+      { 道路名稱: "內湖路" },
+      { 道路名稱: "成功路" },
+      { 道路名稱: "康寧路" },
+      { 道路名稱: "行善路" },
+      { 道路名稱: "東湖路" },
+      { 道路名稱: "文湖街" },
+      { 道路名稱: "新湖三路" },
+      { 道路名稱: "港墘路" },
+    ],
+  },
+  {
+    區域: "南港區",
+    road: [
+      { 道路名稱: "經貿二路" },
+      { 道路名稱: "南港路" },
+      { 道路名稱: "三重路" },
+      { 道路名稱: "成福路" },
+      { 道路名稱: "玉成街" },
+      { 道路名稱: "忠孝東路" },
+      { 道路名稱: "市民大道" },
+      { 道路名稱: "東新街" },
+    ],
+  },
+  {
+    區域: "文山區",
+    road: [
+      { 道路名稱: "木新路" },
+      { 道路名稱: "興隆路" },
+      { 道路名稱: "羅斯福路" },
+      { 道路名稱: "景文街" },
+      { 道路名稱: "辛亥路" },
+      { 道路名稱: "指南路" },
+      { 道路名稱: "試院路" },
+      { 道路名稱: "木柵路" },
+      { 道路名稱: "景興路" },
+      { 道路名稱: "試院街" },
+    ],
+  },
+];
+
 const area = document.querySelector("#area");
+const road = document.querySelector("#road");
 function filterData(data) {
   area.addEventListener("change", function (e) {
     let newData = [];
     if (e.target.value === "") {
+      road.innerHTML = "";
       renderData(originalData);
       return;
     }
     data.forEach(function (item, index) {
       if (e.target.value === item.sarea) {
         newData.push(item);
+        taipei_roads.forEach(function (item) {
+          if (item.區域 == e.target.value) {
+            let str = "";
+            item.road.forEach(function (item) {
+              content = `<option value="${item.道路名稱}">${item.道路名稱}</option>`;
+              str += content;
+            });
+            road.innerHTML = `<option value="全部">全部</option>${str}`;
+          }
+        });
       }
       renderData(newData);
       mrtStation.value = "";
     });
+  });
+}
+
+function renderRoadSelect(data) {
+  road.addEventListener("change", function (e) {
+    if (area.value == "") {
+      renderData(data);
+      return;
+    }
+    let newRoadData = [];
+    data.forEach(function (item) {
+      let keyword = e.target.value;
+      if (keyword == "全部" && area.value == item.sarea) {
+        newRoadData.push(item);
+      }
+
+      if (item.ar.includes(keyword) || item.sarea.includes(keyword)) {
+        if (area.value == item.sarea || item.sarea.includes(keyword)) {
+          newRoadData.push(item);
+        }
+      }
+    });
+    renderData(newRoadData);
+    mrtStation.value = "";
   });
 }
 
@@ -131,11 +368,12 @@ function renderMrtList(data) {
     data.forEach(function (item, index) {
       if (item.sna.match(keyword)) {
         targetStation.push(item);
-        console.log(item);
+        // console.log(item);
       }
     });
     renderData(targetStation);
     area.value = "";
+    road.innerHTML = "";
   });
 }
 
@@ -167,9 +405,9 @@ function keywordSearch(arr) {
     let targetStation = [];
     arr.forEach(function (item, index) {
       if (
-        item.ar.match(keyword) ||
-        item.sarea.match(keyword) ||
-        item.sna.match(keyword)
+        item.ar.includes(keyword) ||
+        item.sarea.includes(keyword) ||
+        item.sna.includes(keyword)
       ) {
         targetStation.push(item);
       }
@@ -177,5 +415,6 @@ function keywordSearch(arr) {
     renderData(targetStation);
     area.value = "";
     mrtStation.value = "";
+    road.value = "全部";
   });
 }
---
+++ Javascript/map.js
@@ -0,0 +1,38 @@
+axios
+  .get(
+    "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+  )
+  .then(function (response) {
+    renderMap(response.data);
+  });
+
+//渲染站點到地圖
+function renderMap(data) {
+  map = L.map("map").setView(userPosition, 17);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  let markers = L.markerClusterGroup().addTo(map);
+  data.forEach(function (item) {
+    markers.addLayer(
+      L.marker([item.lat, item.lng])
+        .bindPopup(
+          `<p>站名：${item.sna}</p><p>地址：${item.ar}</p><p>可租借車輛：${item.tot}</p><p>可歸還車位：${item.sbi}</p>`
+        )
+        .openPopup()
+    );
+    map.addLayer(markers);
+  });
+}
+
+//擷取使用者位置
+let userPosition = [];
+const successCallback = (position) => {
+  userPosition.push(position.coords.latitude, position.coords.longitude);
+};
+const errorCallback = (error) => {
+  console.log(error);
+};
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
