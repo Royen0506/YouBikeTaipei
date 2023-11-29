4d881e672f174086af8ef35b886633e3977b8536
Roy <f0980774144@gmail.com>
2023-11-29 18:16:11 +0800

    優化導航轉址

--- Javascript/map.js
+++ Javascript/map.js
@@ -82,12 +82,14 @@ function renderMap(data, userPosition) {
       mask = greenIcon;
     }
 
-    let arKeyword = `${item.sna.replace("_", "微笑單車2.0:")}`;
+    let arKeyword = `${item.sna
+      .replace("2.0", "微笑單車+2.0:")
+      .replace("_", "")}`;
 
     markers.addLayer(
       L.marker([item.lat, item.lng], { icon: mask })
         .bindPopup(
-          `<p>站名：${item.sna}</p><p>地址：${item.ar}</p><p>可租借車輛：${item.sbi}</p><p>可歸還車位：${item.bemp}</p><a target="_blank" href="https://www.google.com.tw/maps/dir/${userPosition}/${arKeyword}/@${item.lat},${item.lng},21z/?entry=ttu">在Google Map上導航</a>`
+          `<p>站名：${item.sna}</p><p>地址：${item.ar}</p><p>可租借車輛：${item.sbi}</p><p>可歸還車位：${item.bemp}</p><a target="_blank" href="https://www.google.com.tw/maps/dir/${userPosition}/${arKeyword}/@${item.lat},${item.lng},19z/?entry=ttu">在Google Map上導航</a>`
         )
         .openPopup()
     );
